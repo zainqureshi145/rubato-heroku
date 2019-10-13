63a408033668638d1c11d3e529116b370dee70f5
zainqureshi145 <zainqureshi145@gmail.com>
2019-10-14 01:00:45 +0200

    Added React to Project

---
+++ src/main/resources/static/precache-manifest.5280c049a1d93f7de46975a91d6d7e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adce0a984b2fd6b9f19f460cc3464e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cffd9ab601a5e5869b9",
+    "url": "/static/css/main.b56aec37.chunk.css"
+  },
+  {
+    "revision": "cb38612e33e9b4a69093",
+    "url": "/static/js/2.4fb3b906.chunk.js"
+  },
+  {
+    "revision": "3cffd9ab601a5e5869b9",
+    "url": "/static/js/main.a98e20ef.chunk.js"
+  },
+  {
+    "revision": "9fddd2d914a9e35a6cc2",
+    "url": "/static/js/runtime~main.22cd000d.js"
+  }
+]);
